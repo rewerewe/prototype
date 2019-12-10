b4c93917ba376080db62f2bf897237822ac45dad
idealful <idealful@gmail.com>
2019-12-10 10:39:02 +0900

    Rename @SpringBootApplication class name

--- demoRestTemplate/src/main/java/com/example/demo/Application.java
+++ demoRestTemplate/src/main/java/com/example/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiServerToServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiServerToServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
